30ae884e4b634ea68f64441c516b89f504cb1443
root <aobersnel@apnex.com.au>
2020-04-23 15:23:13 +1000

    Updates...

--- make.iso.sh
+++ make.iso.sh
@@ -39,6 +39,7 @@ IPXE=$2
 if [[ -n "${FILE}" ]]; then
 	# setup custom ipxe flags
 	cat <<-EOF > ${IPXEDIR}/config/local/general.h
+		#define PARAM_CMD
 		#define CONSOLE_CMD
 		#define IMAGE_COMBOOT
 		#define IMAGE_PNG
