6d867e59ce96b873f91516d3c3ca7f2ee30aae8c
root <aobersnel@apnex.com.au>
2020-09-25 17:43:07 +1000

    Updates...

--- centos.docker/runonce.sh
+++ centos.docker/runonce.sh
@@ -2,8 +2,10 @@
 
 if [[ -e /tmp/runonce ]]; then
 	rm /tmp/runonce
+	echo "TESTING" >> logger
 	exec > /root/runonce.log 2>&1
 	curl -fsSL http://labops.sh/docker/install | sh
+	echo "FINISHED"
 fi
 
 exit
