a665b5a0586e3de2a72dc4b64cc8a2a3439e7c3f
root <aobersnel@apnex.com.au>
2020-10-15 15:30:33 +1100

    Updates..

--- make.iso.sh
+++ make.iso.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-## prerequisites ## dnf install xz-devel make
+## prerequisites ##
+# yum install -y xz-devel make gcc
 # detect and resolve symlink
 if [[ $(readlink -f $0) =~ ^(.*)/([^/]+)$ ]]; then
 	WORKDIR="${BASH_REMATCH[1]}"
